5628b88ce3c1a1dfa9a8a6608e1b7c5e597e95cf
junioranheu <junioranheu@gmail.com>
2025-02-28 23:00:12 -0300

    wip ClientController

---
+++ Orquestra.API/Controllers/ClientController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orquestra.API.Filters;
+using Orquestra.Application.UseCases.Clients.Create;
+using Orquestra.Application.UseCases.Clients.Get;
+using Orquestra.Application.UseCases.Clients.Shared;
+
+namespace Orquestra.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClientController(IGetClient get, ICreateClient create) : BaseController<ClientController>
+{
+    private readonly IGetClient _get = get;
+    private readonly ICreateClient _create = create;
+
+    [AuthorizeFilter]
+    [HttpPost]
+    public async Task<ActionResult<ClientOutput>> Create([FromForm] ClientInput input)
+    {
+        Guid userId = GetUserId(throwExceptionIfNotAuth: true);
+        ClientOutput output = await _create.Execute(userId, input);
+
+        return output;
+    }
+
+    [AuthorizeFilter]
+    [HttpGet]
+    public async Task<ActionResult<ClientOutput?>> Get(Guid ClientId)
+    {
+        ClientOutput? output = await _get.Execute(ClientId);
+        return output;
+    }
+
+    [AuthorizeFilter]
+    [HttpGet("GetAll")]
+    public async Task<ActionResult<List<ClientOutput>?>> GetAll()
+    {
+        List<ClientOutput>? output = await _get.Execute();
+        return output;
+    }
+}
