566cec0045ddd87d84c982f6f366c4ca3cba55b1
junioranheu <junioranheu@gmail.com>
2025-08-19 21:23:38 -0300

    add SystemConsts

--- Orquestra.Domain/Consts/SystemConsts.cs
+++ Orquestra.Domain/Consts/SystemConsts.cs
@@ -4,6 +4,8 @@ public static class SystemConsts
 {
     public const string Name = "Orquestra.API";
     public const string Email = "orquestra@gmail.com";
+    public const string Author = "@junioranheu";
+    public const string Slogan = "Orquestra: Harmonia na gestão do seu negócio";
 
     public const int OneMinuteInSec = 60;
     public const int TenMinutesInSec = 600;
