b73d2e430f19dae67c9d6e793b33b2ad5246415c
junioranheu <junioranheu@gmail.com>
2025-08-22 18:07:37 -0300

    wip front-end initial structure

--- Orquestra.Frontend/app/(routes)/head.tsx
+++ Orquestra.Frontend/app/(routes)/head.tsx
@@ -9,7 +9,7 @@ export default function Head() {
             <meta name='author' content={SYSTEM.AUTHOR} />
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta name='theme-color' content={SYSTEM.COLOR} />
-            <link rel='icon' href='/favicon.svg' />
+            <link rel='icon' href='/favicon.ico' />
         </Fragment>
     )
 }
