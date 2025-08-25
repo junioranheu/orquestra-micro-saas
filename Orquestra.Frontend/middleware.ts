157cfe73fb6b5bfad28813d59dc7edd02bcda74b
junioranheu <junioranheu@gmail.com>
2025-08-25 20:34:11 -0300

    refactor middleware

--- Orquestra.Frontend/middleware.ts
+++ Orquestra.Frontend/middleware.ts
@@ -4,30 +4,23 @@ import ROUTES from './app/consts/routes';
 const PUBLIC_PATHS = [ROUTES.ENTRAR, ROUTES.CRIAR_CONTA, '/public'];
 
 export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
     const token = request.cookies.get('auth')?.value;
-    // console.log(`cookies.value ${token}`);
+    const pathname = request.nextUrl.pathname;
+    const isPublicPath = PUBLIC_PATHS.some(path => pathname.startsWith(path));
 
-    // Redireciona '/' para dashboard;
-    if (pathname === '/') {
+    // Sem token: só permite rotas públicas;
+    if (!token) {
+        return isPublicPath ? NextResponse.next() : NextResponse.redirect(new URL(ROUTES.ENTRAR, request.url));
+    }
+
+    // Com token: redireciona home ('/') e rotas públicas para dashboard;
+    if (pathname === '/' || isPublicPath) {
         return NextResponse.redirect(new URL(ROUTES.DASHBOARD, request.url));
     }
 
-    // Se tiver token e tentar acessar rota de "não auth", redireciona pro dashboard;
-    if (token && PUBLIC_PATHS.some(path => pathname.startsWith(path))) {
-        return NextResponse.redirect(new URL(ROUTES.DASHBOARD, request.url));
-    }
-
-    // Se não tiver token e tentar acessar rota protegida, redireciona pro login;
-    if (!token && !PUBLIC_PATHS.some(path => pathname.startsWith(path))) {
-        return NextResponse.redirect(new URL(ROUTES.ENTRAR, request.url));
-    }
-
-    // Se tiver token e rota protegida, ou rota pública sem token → deixa passar;
     return NextResponse.next();
 }
 
 export const config = {
-    runtime: 'nodejs',
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
