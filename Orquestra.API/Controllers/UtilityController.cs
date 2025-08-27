4468b4f2ada5898e5d0afb108f941a3da6c1b144
junioranheu <junioranheu@gmail.com>
2025-08-27 20:40:03 -0300

    dev end-point GetBuildVersion

--- Orquestra.API/Controllers/UtilityController.cs
+++ Orquestra.API/Controllers/UtilityController.cs
@@ -4,6 +4,7 @@ using Orquestra.Application.UseCases.Locations.Cities.Get;
 using Orquestra.Application.UseCases.Locations.States.Get;
 using Orquestra.Domain.Consts;
 using Orquestra.Domain.Entities;
+using System.Reflection;
 
 namespace Orquestra.API.Controllers;
 
@@ -13,6 +14,28 @@ public class UtilityController(IGetState getState, IGetCity getCity) : BaseContr
 {
     private readonly IGetState _getState = getState;
     private readonly IGetCity _getCity = getCity;
+
+    [AllowAnonymous]
+    [HttpGet("GetBuildVersion")]
+    public ActionResult GetBuildVersion()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        string version = assembly.GetName().Version?.ToString() ?? "-";
+
+        var build = new
+        {
+            BuildVersion = version,
+            AssemblyName = assembly.GetName().Name,
+            Configuration =
+#if DEBUG
+            "Debug"
+#else
+            "Release"
+#endif
+        };
+
+        return Ok(build);
+    }
 
     [ResponseCache(Duration = SystemConsts.OneMonthInSec)]
     [AllowAnonymous]
