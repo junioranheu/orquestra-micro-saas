3aa0dfb77b81b6028fadcd8b1dbf81c4924299b2
junioranheu <junioranheu@gmail.com>
2025-09-04 15:21:26 -0300

    add electron

--- Orquestra.Frontend/electron/main.js
+++ Orquestra.Frontend/electron/main.js
@@ -1,16 +1,15 @@
 import { app, BrowserWindow } from 'electron';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1280,
         height: 800,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: false,
             contextIsolation: true,
         },
