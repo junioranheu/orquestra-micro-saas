b522ee17bf0ccf3841032921281abd641f6401ad
junioranheu <junioranheu@gmail.com>
2025-09-15 01:27:51 -0300

    wip Navbar.tsx

--- Orquestra.Frontend/app/components/navbar/nav/index.tsx
+++ Orquestra.Frontend/app/components/navbar/nav/index.tsx
@@ -1,7 +1,6 @@
 import Icon from '@/app/components/icon';
 import ROUTES from '@/app/consts/routes';
 import useApiGetMe from '@/app/hooks/api/useApiGetMe';
-import useUserContext from '@/app/hooks/contexts/useUserContext';
 import Tippy from '@tippyjs/react';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -10,35 +9,32 @@ import styles from './index.module.scss';
 export default function Navbar() {
 
     const router = useRouter();
-    const [auth, setAuth] = useUserContext();
     const me = useApiGetMe();
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-
-    function handleRedirect() {
-        router.push(auth ? ROUTES.DASHBOARD : ROUTES.LOGIN);
-    }
 
     return (
         <nav className={styles.nav}>
             <div className={styles.inner}>
                 <div className={styles.right}>
                     <Tippy content="Entenda mais sobre plano atual e explore novos">
-                        <span><Icon icon='tag' weight='bold' /> Uso e plano</span>
+                        <span onClick={() => router.push(ROUTES.DASHBOARD)}><Icon icon='tag' weight='bold' /> Uso e plano</span>
                     </Tippy>
 
                     <Tippy content="Ajuda">
-                        <span><Icon icon='help-circle' weight='bold' /></span>
+                        <span onClick={() => router.push(ROUTES.DASHBOARD)}><Icon icon='help-circle' weight='bold' /></span>
                     </Tippy>
-
 
                     <Tippy content="Notificações">
-                        <span><Icon icon='bell' weight='bold' /></span>
+                        <span onClick={() => router.push(ROUTES.DASHBOARD)}><Icon icon='bell' weight='bold' /></span>
                     </Tippy>
-
 
                     <Tippy content="Gerencie seu perfil, plano, configurações e muito mais.">
-                        <span><Icon icon='briefcase' weight='bold' />{me && me?.currentMainCompany ? me.currentMainCompany?.name : 'Olá, tudo bem?'} <Icon icon='chevron-down' weight='bold' /></span>
+                        <span onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                            <Icon icon='briefcase' weight='bold' />{me && me?.currentMainCompany ? me.currentMainCompany?.name : 'Olá, tudo bem?'} <Icon icon='chevron-down' weight='bold' />
+                        </span>
                     </Tippy>
+
+                    <h1>{isMenuOpen.toString()}</h1>
                 </div>
             </div>
         </nav>
