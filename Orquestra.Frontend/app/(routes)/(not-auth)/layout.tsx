d2c8c4334a5f61a63f7000b88211a9b16d74790e
junioranheu <junioranheu@gmail.com>
2025-09-18 19:01:12 -0300

    add hook useCheckAzureServer

--- Orquestra.Frontend/app/(routes)/(not-auth)/layout.tsx
+++ Orquestra.Frontend/app/(routes)/(not-auth)/layout.tsx
@@ -4,12 +4,16 @@ import UpNav from '@/app/components/navbar/up-nav';
 import { GlobalContextProvider } from '@/app/contexts/global.context';
 import { UserProvider } from '@/app/contexts/user.context';
 import { HANKEN } from '@/app/fonts/fonts';
+import useCheckAzureServer from '@/app/hooks/useCheckAzureServer';
 import '@/app/styles/globals.scss';
 import 'animate.css/animate.min.css';
 import { ReactNode } from 'react';
 import { Toaster } from 'sonner';
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+
+    useCheckAzureServer();
+
     return (
         <html lang='pt-BR'>
             <UserProvider>
