9afd8b146e4b968ec5ddb908257fe94f405542b7
junioranheu <junioranheu@gmail.com>
2025-09-17 22:50:33 -0300

    wip NotFound.tsx

--- Orquestra.Frontend/app/not-found.tsx
+++ Orquestra.Frontend/app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client';
 import '@/app/styles/globals.scss';
-import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import LayoutTemplateOne from './components/layout/template-one';
 import { HANKEN } from './fonts/fonts';
 import useTitle from './hooks/useTitle';
@@ -8,7 +8,11 @@ import useTitle from './hooks/useTitle';
 export default function NotFound() {
 
     useTitle('Página não encontrada');
-    const pathname = usePathname();
+    const [path, setPath] = useState<string>('');
+
+    useEffect(() => {
+        setPath(window.location.pathname);
+    }, []);
 
     return (
         <div className={HANKEN.className}>
@@ -16,7 +20,7 @@ export default function NotFound() {
                 svg='error'
                 code='#404'
                 title='Página não encontrada'
-                description={`A página "${pathname}" que você tentou acessar não existe (ou está de férias).`}
+                description={path ? `A página "${path}" que você tentou acessar não existe (ou está de férias 🏖️).` : 'A página que você tentou acessar não existe (ou está de férias 🏖️).'}
                 showSupportContact={true}
             />
         </div>
