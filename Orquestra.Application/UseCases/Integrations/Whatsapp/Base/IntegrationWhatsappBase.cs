37aa1ee491be86d3c0207c0c10255eb26ac1888a
junioranheu <junioranheu@gmail.com>
2025-10-28 17:50:27 -0300

    wip IntegrationWhatsappBase

--- Orquestra.Application/UseCases/Integrations/Whatsapp/Base/IntegrationWhatsappBase.cs
+++ Orquestra.Application/UseCases/Integrations/Whatsapp/Base/IntegrationWhatsappBase.cs
@@ -6,6 +6,7 @@ using Orquestra.Domain.Consts;
 using Orquestra.Domain.Enums;
 using Orquestra.Infrastructure.Data;
 using static Orquestra.Utils.Fixtures.Get;
+using static Orquestra.Utils.Fixtures.RegexPatterns;
 
 namespace Orquestra.Application.UseCases.Integrations.Whatsapp.Base;
 
@@ -28,6 +29,10 @@ public partial class IntegrationWhatsappBase(IntegrationWhatsappBaseDependencies
         {
             throw new InvalidOperationException(SystemConsts.Warnings.NotLinkedOrDontHaveCompany);
         }
+        else
+        {
+            input.CompanyName = currentMainCompany.Name;
+        }
 
         if (currentMainCompany.PlanType == PlanTypeEnum.Free)
         {
@@ -39,6 +44,33 @@ public partial class IntegrationWhatsappBase(IntegrationWhatsappBaseDependencies
             throw new InvalidOperationException($"Sua empresa está na situação de <b>{GetEnumDesc(CompanySituationEnum.PendingPayment).ToLowerInvariant}</b>, portanto não pode prosseguir com esta ação.");
         }
 
+        if (!IsPhoneValid(input.Phone))
+        {
+            throw new ArgumentException("O número de telefone não é válido. Insira um número válido, por favor.");
+        }
+
+        if (string.IsNullOrEmpty(input.Message))
+        {
+            throw new ArgumentException("O conteúdo da mensagem está vazio. Insira texto válido, por favor.");
+        }
+
+        if (!IsFullNameValid(input.FromFullName))
+        {
+            throw new ArgumentException("O nome não é válido. Insira um nome completo, por favor.");
+        }
+
         await _checkIfUserIsLinkedCompanyUser.Execute(companyId: currentMainCompany.CompanyId, userId: userIdAuth, needCompanyAdmin: false);
     }
+
+    #region extras
+    private static bool IsFullNameValid(string fullName)
+    {
+        return RegexName().IsMatch(fullName);
+    }
+
+    private static bool IsPhoneValid(string? phone)
+    {
+        return RegexPhone().IsMatch(phone ?? string.Empty);
+    }
+    #endregion
 }
--- Orquestra.Application/UseCases/Integrations/Whatsapp/Shared/IntegrationWhatsappMessageInput.cs
+++ Orquestra.Application/UseCases/Integrations/Whatsapp/Shared/IntegrationWhatsappMessageInput.cs
@@ -1,4 +1,7 @@
-﻿namespace Orquestra.Application.UseCases.Integrations.Whatsapp.Shared;
+﻿using static Orquestra.Utils.Fixtures.Get;
+
+
+namespace Orquestra.Application.UseCases.Integrations.Whatsapp.Shared;
 
 public sealed class IntegrationWhatsappMessageInput
 {
@@ -6,7 +9,9 @@ public sealed class IntegrationWhatsappMessageInput
 
     public required string Message { get; set; }
 
-    public required string From { get; set; }
+    public required string FromFullName { get; set; }
 
-    public DateTime DateEnd { get; set; }
+    public string? CompanyName { get; set; } = string.Empty;
+
+    public DateTime Date { get; set; } = GetDate();
 }
