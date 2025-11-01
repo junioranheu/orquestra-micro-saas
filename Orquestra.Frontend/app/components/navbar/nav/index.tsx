434dcb0092e5acccc8505b2ec91571398e668683
junioranheu <junioranheu@gmail.com>
2025-11-01 18:33:44 -0300

    general improvements

--- Orquestra.Frontend/app/components/navbar/nav/index.tsx
+++ Orquestra.Frontend/app/components/navbar/nav/index.tsx
@@ -16,6 +16,7 @@ export default function Navbar() {
 
     const router = useRouter();
     const me = useApiGetMe({});
+
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
     const [modalPosition, setModalPosition] = useState<iModalCustomPosition>({});
 
